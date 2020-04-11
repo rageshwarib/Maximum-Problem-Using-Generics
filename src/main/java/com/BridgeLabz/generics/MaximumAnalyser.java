ab0deca9f7c04541652f9b8a7f12e648af2cec6d
rageshwari <ragu.bodke@gmail.com>
2020-04-11 15:29:43 +0530

    Refactored:all the three to One Generic Method and find the maximum

--- src/main/java/com/BridgeLabz/generics/MaximumAnalyser.java
+++ src/main/java/com/BridgeLabz/generics/MaximumAnalyser.java
@@ -2,7 +2,8 @@ package com.BridgeLabz.generics;
 
 public class MaximumAnalyser {
 
-    public Integer FindMaximum(Integer first, Integer second, Integer third) {
+    public <T extends Comparable<T>> T findMaximum(T first, T second, T third)
+    {
         if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
             return first;
         else if (second.compareTo(first) > 0 && second.compareTo(third) > 0)
@@ -10,22 +11,7 @@ public class MaximumAnalyser {
         else
             return third;
     }
-    public Float FindMaximum(Float first, Float second, Float third) {
-        if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
-            return first;
-        else if (second.compareTo(first) > 0 && second.compareTo(third) > 0)
-            return second;
-        else
-            return third;
-    }
-    public String FindMaximum(String firstString, String secondString, String thirdString) {
-        if (firstString.compareTo(secondString) > 0 && firstString.compareTo(thirdString) > 0)
-            return firstString;
-        else if (secondString.compareTo(firstString) > 0 && secondString.compareTo(thirdString) > 0)
-            return secondString;
-        else
-            return thirdString;
-    }
+
 }
 
 
--- src/test/java/com/BridgeLabz/generics/MaximumAnalyserTests.java
+++ src/test/java/com/BridgeLabz/generics/MaximumAnalyserTests.java
@@ -7,48 +7,48 @@ public class MaximumAnalyserTests {
     MaximumAnalyser maximumAnalyser = new MaximumAnalyser();
     @Test
     public void GivenThreeIntegers_findMaximumAtfirstPosition_shuldReturnSameNumber() {
-        Integer maximumNumber = maximumAnalyser.FindMaximum(67,22, 30);
+        Integer maximumNumber = maximumAnalyser.findMaximum(67,22, 30);
         Assert.assertEquals((Integer) 67 , maximumNumber);
     }
     @Test
     public void GivenThreeIntegers_findMaximumAt_secondPosition_shuldReturnSameNumber() {
-        Integer maximumNumber = maximumAnalyser.FindMaximum(22 ,67, 30);
+        Integer maximumNumber = maximumAnalyser.findMaximum(22 ,67, 30);
         Assert.assertEquals((Integer) 67 , maximumNumber);
     }
     @Test
     public void GivenThreeIntegers_findMaximumAt_thirdPosition_shuldReturnSameNumber() {
-        Integer maximumNumber = maximumAnalyser.FindMaximum(30 ,22, 67);
+        Integer maximumNumber = maximumAnalyser.findMaximum(30 ,22, 67);
         Assert.assertEquals((Integer) 67 , maximumNumber);
     }
 
     @Test
     public void GivenThreeFloat_findMaximumAt_firstPosition_shuldReturnSameNumber() {
-        Float maximumNumber = maximumAnalyser.FindMaximum(67.25f,22.25f, 30.65f);
+        Float maximumNumber = maximumAnalyser.findMaximum(67.25f,22.25f, 30.65f);
         Assert.assertEquals((Float) 67.25f , maximumNumber);
     }
     @Test
-    public void GivenThreeFloat_findMaximumAt_secondPosition_shuldReturnSameNumber() {
-        Float maximumNumber = maximumAnalyser.FindMaximum(22.25f ,67.25f, 30.65f);
+    public void GivenThreeFloatNumbers_findMaximumAt_secondPosition_shuldReturnSameNumber() {
+        Float maximumNumber = maximumAnalyser.findMaximum(22.25f ,67.25f, 30.65f);
         Assert.assertEquals((Float) 67.25f , maximumNumber);
     }
     @Test
-    public void GivenThreeFloat_findMaximumAt_thirdPosition_shuldReturnSameNumber() {
-        Float maximumNumber = maximumAnalyser.FindMaximum(30.65f ,22.25f, 67.25f);
+    public void GivenThreeFloatNumbers_findMaximumAt_thirdPosition_shuldReturnSameNumber() {
+        Float maximumNumber = maximumAnalyser.findMaximum(30.65f ,22.25f, 67.25f);
         Assert.assertEquals((Float) 67.25f , maximumNumber);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_firstPosition_shuldReturnSameString() {
-        String maximumNumber = maximumAnalyser.FindMaximum("Peach" , "Apple" , "Banana");
+        String maximumNumber = maximumAnalyser.findMaximum("Peach" , "Apple" , "Banana");
         Assert.assertEquals((String) "Peach" , maximumNumber);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_secondPosition_shuldReturnSameString() {
-        String maximumNumber = maximumAnalyser.FindMaximum("Apple","Peach" ,"Banana");
+        String maximumNumber = maximumAnalyser.findMaximum("Apple","Peach" ,"Banana");
         Assert.assertEquals((String) "Peach" , maximumNumber);
     }
     @Test
     public void GivenThreeStrings_findMaximumAt_thirdPosition_shuldReturnSameString() {
-        String maximumNumber = maximumAnalyser.FindMaximum("Banana" ,"Apple", "Peach");
+        String maximumNumber = maximumAnalyser.findMaximum("Banana" ,"Apple", "Peach");
         Assert.assertEquals((String) "Peach" , maximumNumber);
     }
 
