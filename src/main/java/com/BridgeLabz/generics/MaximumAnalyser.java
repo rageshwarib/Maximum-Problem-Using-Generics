4a693cfe25e03dbb07cb8fe0276f5a1dfc2faee1
rageshwari <ragu.bodke@gmail.com>
2020-04-11 16:05:40 +0530

    Refactored:code to create Generic Class to take in three variables of Generic Type

--- src/main/java/com/BridgeLabz/generics/MaximumAnalyser.java
+++ src/main/java/com/BridgeLabz/generics/MaximumAnalyser.java
@@ -1,8 +1,22 @@
 package com.BridgeLabz.generics;
 
-public class MaximumAnalyser {
+public class MaximumAnalyser<T extends Comparable<T>> {
 
-    public <T extends Comparable<T>> T findMaximum(T first, T second, T third)
+    private T first;
+    private T second;
+    private T third;
+
+    public MaximumAnalyser() {
+    }
+    //Parameterized Constructor
+    public MaximumAnalyser(T first, T second, T third){
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+
+    //Generic Method for finding Maximum from three elements
+    public T findMaximum(T first, T second, T third)
     {
         if (first.compareTo(second) > 0 && first.compareTo(third) > 0)
             return first;
@@ -11,7 +25,9 @@ public class MaximumAnalyser {
         else
             return third;
     }
-
+    public <T extends Comparable<T>> T findMaximum(){
+        return (T) findMaximum(first, second, third);
+    }
 }
 
 
